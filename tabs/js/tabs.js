f1eb37c2665284217248e69865095cd8a0799d1c
stdemon <dmitriy.krasnikov@gmail.com>
2012-07-11 22:41:16 +0300

    added new feature

--- tabs/js/tabs.js
+++ tabs/js/tabs.js
@@ -1,8 +1,16 @@
 function tabClick() {
     this.onclick = function(event)
-    {
+    {   
+        var divs = document.getElementsByTagName("div");
+        for(var i=0;i<divs.length;i++)
+            {
+             if(divs[i].className == "tab active")   
+                 {
+                     divs[i].className = "tab";
+                 }
+            }
         t=event.target||event.srcElement;
-        if(t.className == "tab" || t.tagName == "SPAN")
+        if(t.className == "tab")
             {
                 t.className = "tab active";
             }
